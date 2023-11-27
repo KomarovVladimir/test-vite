4c9367182c601a70c14d81df0ed1c2e68adeb052
KomarovVladimir <komarov2142@gmail.com>
2023-11-27 16:19:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "https://komarovvladimir.github.io/",
     plugins: [react(), tsconfigPaths()],
 });
