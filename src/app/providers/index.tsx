532254637f6da88e58bad26d3baa0e8aca5373fa
KomarovVladimir <komarov2142@gmail.com>
2023-11-27 15:54:40 +0300

    Fix

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,17 +1,20 @@
 import { Provider as ReduxProvider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter } from "react-router-dom";
 
 import { appStore } from "app/appStore";
+import { Page404 } from "pages/page404";
+
 import { Router } from "./RouterProvider";
 
 export const Provider = () => {
     return (
-        // <ErrorBoundary FallbackComponent={FullPageError}>
-        <ReduxProvider store={appStore}>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
-        </ReduxProvider>
-        //</ErrorBoundary>
+        <ErrorBoundary FallbackComponent={Page404}>
+            <ReduxProvider store={appStore}>
+                <BrowserRouter>
+                    <Router />
+                </BrowserRouter>
+            </ReduxProvider>
+        </ErrorBoundary>
     );
 };
