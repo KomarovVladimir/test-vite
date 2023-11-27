c6db4238980bc104429edc89f5ddc9cb751d0146
KomarovVladimir <komarov2142@gmail.com>
2023-11-27 17:36:09 +0300

    Debug

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -41,10 +41,10 @@ export const Router = () => {
                         { path: ":id", element: <ArticlePage /> },
                     ],
                 },
-                {
-                    path: "*",
-                    element: <Navigate to={PATH_PAGE.page404} replace />,
-                },
+                // {
+                //     path: "*",
+                //     element: <Navigate to={PATH_PAGE.page404} replace />,
+                // },
             ],
         },
     ]);
